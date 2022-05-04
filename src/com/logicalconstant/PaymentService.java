a4b63664d3a73f1c7cb45cc670bdd482fb62db8f
Manodyna K H <77208955+manodyna@users.noreply.github.com>
2022-05-04 12:31:22 +0530

    Update PaymentService.java

--- src/com/logicalconstant/PaymentService.java
+++ src/com/logicalconstant/PaymentService.java
@@ -2,7 +2,8 @@ package com.logicalconstant;
 
 public class PaymentService implements Services{
     public static void makePayment(String userId, String productId){
-        System.out.println("[PaymentService]charging user "+userId+" for product "+productId);
+        System.out.println("[PaymentService]charging user "
+                           +userId+" for product "+productId);
 //        complex logic for making payment
     }
 }
