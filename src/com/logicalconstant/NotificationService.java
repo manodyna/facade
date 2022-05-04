60b868195469004487c8f50fce600abfb1b7c7d1
Manodyna K H <77208955+manodyna@users.noreply.github.com>
2022-05-04 12:30:54 +0530

    Update NotificationService.java

--- src/com/logicalconstant/NotificationService.java
+++ src/com/logicalconstant/NotificationService.java
@@ -2,7 +2,8 @@ package com.logicalconstant;
 
 public class NotificationService implements Services{
     public static void notifyDealer(String productId){
-        System.out.println("[NotificationService] notifying dealer about the sale of the product "+productId);
+        System.out.println("[NotificationService] notifying"+
+                           "dealer about the sale of the product "+productId);
 //        complex notification logic
     }
 }
