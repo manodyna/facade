a77b87cc558c8e294ed1dde938c73ccdbebd624f
Manodyna K H <77208955+manodyna@users.noreply.github.com>
2022-05-04 12:31:52 +0530

    Update ProductService.java

--- src/com/logicalconstant/ProductService.java
+++ src/com/logicalconstant/ProductService.java
@@ -3,11 +3,13 @@ package com.logicalconstant;
 public class ProductService{
     static void assignProductToUser(String productId, String userId){
 //        complex code for product assignment
-        System.out.println("[ProductService]assigning "+productId+" to user "+userId);
+        System.out.println("[ProductService]assigning "+productId
+                           +" to user "+userId);
     }
     static boolean isProductAvailable(String productId){
 //        complex code for checking product availability
-        System.out.println("[ProductService]checking availability of "+productId);
+        System.out.println("[ProductService]checking availability of "
+                           +productId);
         return true;
     }
 }
