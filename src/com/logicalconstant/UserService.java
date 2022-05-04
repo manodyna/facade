c03c131418f567e8e84f9ab0304120ff706b8a73
Manodyna K H <77208955+manodyna@users.noreply.github.com>
2022-05-04 12:33:48 +0530

    Update UserService.java

--- src/com/logicalconstant/UserService.java
+++ src/com/logicalconstant/UserService.java
@@ -1,6 +1,7 @@
 package com.logicalconstant;
 
 public class UserService implements Services{
+    @Override
     public static boolean isUserValid(String userId){
         System.out.println("[UserService]validating user ID, "+userId);
 //        complex code for checking product availability
